2b555ec9de2afd7cf8018381b8ee3c6fe48d0693
Samir CODESLAYER666 Hani <samir.alkurdi@sanoste.fi>
2017-03-23 23:36:08 +0200

    remove add listeners from for loop. does not make sense. instead place SetupServers at the end and deGo it so the program does not exit

--- main.go
+++ main.go
@@ -6,14 +6,11 @@ import (
 )
 
 func main() {
-	// Start the HTTP and TCP server
-	go test1.SetupServers(":9999", ":8888")
-
 	// each Listen represents a subscriber of the channel and receives the
 	// same command
-	for {
-		test1.Listen(func(cmd test1.Cmd) { fmt.Println(1, cmd) })
-		test1.Listen(func(cmd test1.Cmd) { fmt.Println(2, cmd) })
-		test1.Listen(func(cmd test1.Cmd) { fmt.Println(3, cmd) })
-	}
+	test1.Listen(func(cmd test1.Cmd) { fmt.Println(1, cmd) })
+	test1.Listen(func(cmd test1.Cmd) { fmt.Println(2, cmd) })
+	test1.Listen(func(cmd test1.Cmd) { fmt.Println(3, cmd) })
+	// Start the HTTP and TCP server
+	test1.SetupServers(":9999", ":8888")
 }

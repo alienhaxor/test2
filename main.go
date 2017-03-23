735b7f8ec52773a1302a2d1464bd4ddf0ed38a0a
Samir CODESLAYER666 Hani <samir.alkurdi@sanoste.fi>
2017-03-23 23:25:34 +0200

    Refactored for multiple subscribers

--- main.go
+++ main.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"fmt"
-	_ "github.com/cancerballs/test1"
-	handlers "github.com/cancerballs/test1/handlers"
-	utils "github.com/cancerballs/test1/utils"
-	"net/http"
+	test1 "github.com/cancerballs/test1"
 )
 
 func main() {
-	// start http server
-	go http.ListenAndServe(":9999", nil)
-	// start tcp server
-	go handlers.TcpServer()
+	// Start the HTTP and TCP server
+	go test1.SetupServers(":9999", ":8888")
 
+	// each Listen represents a subscriber of the channel and receives the
+	// same command
 	for {
-		fmt.Println("channel result: ", <-utils.Channel_cmd)
+		test1.Listen(func(cmd test1.Cmd) { fmt.Println(1, cmd) })
+		test1.Listen(func(cmd test1.Cmd) { fmt.Println(2, cmd) })
+		test1.Listen(func(cmd test1.Cmd) { fmt.Println(3, cmd) })
 	}
 }
